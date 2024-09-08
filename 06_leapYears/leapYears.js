5e804ceaf2bdbef3bf7b1a924c01b5d2981da493
RasmusKard <rasmuskard@gmail.com>
2024-09-08 10:43:04 +0300

    exercise complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // if it is divisible by 100 but isnt divisible by 400 then it isnt a leap year
+    // if it is divisible by four but not 100 it is a leap year
+    if (((year % 100 === 0) && (!(year % 400 === 0)))){
+        return false
+    } else if (year % 4 === 0) {
+        return true
+    } else {
+        return false
+    }
+
 
 };
 
