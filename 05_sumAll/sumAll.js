2f9967b1369baf26dbce7611fcead56ea986fc9f
RasmusKard <rasmuskard@gmail.com>
2024-09-08 10:11:36 +0300

    exercise complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(intA, intB) {
+    if (
+        (Number.isInteger(intA) && intA > 0) && 
+        (Number.isInteger(intB) && intB > 0)
+    ) {
+        const endInt = Math.max(intA, intB);
+        // Gaussian sum, hardcoded for start at 1
+        return (endInt * (endInt + 1) / 2)
+    } else {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
