5467c8d7b6a26a74e181f3d4d00a9e4a56248cff
RasmusKard <rasmuskard@gmail.com>
2024-09-08 09:41:48 +0300

    exercise completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...valueToRemove) {
+    const cleanedArray = [];
+    for (const element of array) {
+        if (!(valueToRemove.includes(element))) {
+            cleanedArray.push(element)
+        }
+    }
+
+    return cleanedArray
 };
 
 // Do not edit below this line
