f53714faddd5afbd092dd542a59c070d8edbba8e
RasmusKard <rasmuskard@gmail.com>
2024-09-13 11:00:28 +0300

    exercise complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	str = str.replace(/[\s.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+	str = str.toLowerCase();
+	reverseStr = str.split("").reverse().join("");
+	console.log(reverseStr);
+	console.log(str);
+	return str === reverseStr;
 };
 
 // Do not edit below this line
