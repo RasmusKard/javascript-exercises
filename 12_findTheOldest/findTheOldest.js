badc6b055a03e321da09c9d61b64ac77e31cf356
RasmusKard <rasmuskard@gmail.com>
2024-09-13 12:19:00 +0300

    exercise complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+	people.sort((aPerson, bPerson) => {
+		checkIfHasDeathYear(aPerson);
+		checkIfHasDeathYear(bPerson);
 
+		const aPersonAge = aPerson.yearOfDeath - aPerson.yearOfBirth;
+		const bPersonAge = bPerson.yearOfDeath - bPerson.yearOfBirth;
+
+		return bPersonAge - aPersonAge;
+	});
+	return people[0];
+};
+
+const checkIfHasDeathYear = (person) => {
+	if (!person.yearOfDeath) {
+		person.yearOfDeath = new Date().getFullYear();
+	}
 };
 
 // Do not edit below this line
