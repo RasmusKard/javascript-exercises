92957e614b4e38da0eacf1633240406ed43e571c
RasmusKard <rasmuskard@gmail.com>
2024-09-13 11:37:41 +0300

    exercise complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+	num = parseInt(num);
+	if (num < 0) {
+		return "OOPS";
+	}
+	let fibA = 0;
+	let fibB = 1;
+	let fibSum = 0;
+	for (let i = 0; i < num; i++) {
+		fibSum = fibA + fibB;
+		fibA = fibB;
+		fibB = fibSum;
+	}
+	return fibA;
 };
 
 // Do not edit below this line
